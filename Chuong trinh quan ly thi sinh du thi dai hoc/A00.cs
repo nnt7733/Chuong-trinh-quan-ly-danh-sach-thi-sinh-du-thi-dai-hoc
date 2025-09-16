2d0d50e2c3b469a65ca97991d9ae1b64a4c44df8
nnt7733 <thoain.n2006@gmail.com>
2025-09-16 23:06:26 +0700

    đổi tên

---
+++ Chuong trinh quan ly thi sinh du thi dai hoc/A00.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThiSinhDuThiDaiHoc
+{
+    public class ToHopA00 : ThongTinThiSinh
+    {
+        public override string MaToHop => "A00";
+        public override string TenToHop => "Toán - Lý - Hóa";
+
+        public ToHopA00() : base() { }
+
+        public ToHopA00(string soBD, string hoTen, string gioiTinh,
+                       DateTime ngaySinh, string diaChi)
+            : base(soBD, hoTen, gioiTinh, ngaySinh, diaChi) { }
+
+        protected override void NhapDiemCacMon()
+        {
+            DiemMonHoc["Toán"] = NhapDiemMon("Toán");
+            DiemMonHoc["Lý"] = NhapDiemMon("Lý");
+            DiemMonHoc["Hóa"] = NhapDiemMon("Hóa");
+        }
+
+        public override void InThongTin()
+        {
+            base.InThongTin();
+            Console.WriteLine($"Chi tiết điểm:");
+            foreach (var mon in DiemMonHoc)
+            {
+                Console.WriteLine($"{mon.Key}: {mon.Value}");
+            }
+            Console.WriteLine($"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        }
+    }
+}
