1d843985f73cdd2965abcd53a971daced641b642
nnt7733 <thoain.n2006@gmail.com>
2025-09-16 20:39:45 +0700

    sửa

---
+++ Chuong trinh quan ly thi sinh du thi dai hoc/IThiSinh.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chương_trình_quản_lý_thí_sinh_dự_thi_đại_học
+{
+    public interface IThiSinh
+    {
+        void NhapThongTin();
+        void InThongTin();
+        double TinhTongdiem();
+    }
+}
